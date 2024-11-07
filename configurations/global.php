e1cfce9993e09fd91be994c7c600f70c358b97ee
Luis Gudiel <lggj2310@gmail.com>
2024-11-07 14:56:24 -0600

    DB Configs

---
+++ configurations/global.php
@@ -0,0 +1,11 @@
+<?php
+
+// DB Configuration
+define("DB_HOST", "localhost");
+define("DB_NAME", "db_system");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_ENCODE", "utf-8");
+
+// App name
+define('SYSTEM_NAME', 'POS_SYSTEM');
